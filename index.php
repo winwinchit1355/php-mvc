e9df85a5c3773ba24af1878e65e8bca705db4bec
winwinchit1355 <winwinchit1355@gmail.com>
2023-10-31 23:28:21 +0630

    url override

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
-
-// echo isset($_GET['url'])?$_GET['url']:'test';
-echo $_GET['url'];
+$className = 'Home';
+$url = isset($_GET['url']) ? rtrim($_GET['url'],'/') : '';
+echo $url."<br>";
+$url = explode('/',$url);
+$className = $url[0]??$className;
+$methodName = $url[1]??'';
+echo $className;
+echo "<br>";
+echo $methodName;
+echo "<br>";
+print_r($url);
